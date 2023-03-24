e8cf255087daf1152fca8b318ed8a7523dda8edc
Francisco-Go <franciscogomezperez998@gmail.com>
2023-03-24 01:20:37 -0600

    agregando express.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+/* const app = express()
+
+const port = 5001
+
+app.get('/', (req, res) => {
+    res.send({
+        data: 'Hola mundo'
+    })
+})
+
+app.listen(port, () => {
+    console.log('La aplicación se esta ejecutando');
+}) */
